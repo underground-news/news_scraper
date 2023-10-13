a2b78aca6b1e6c0c2b5111e956c9010b627a861d
Karthick <karthickphysics@gmail.com>
2023-10-13 17:33:12 +0200

    Updated vertex api as well

--- news_api.py
+++ news_api.py
@@ -1,10 +1,25 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 from gnews import GNews
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+
+import vertexai
+from vertexai.language_models import TextGenerationModel
+
+
+vertexai.init(project="merantix-genai23ber-9502", location="us-central1")
+parameters = {
+    "candidate_count": 1,
+    "max_output_tokens": 1024,
+    "temperature": 0.2,
+    "top_p": 0.8,
+    "top_k": 40,
+}
+model = TextGenerationModel.from_pretrained("text-bison")
 
 
 app = FastAPI()
@@ -31,8 +46,8 @@ def get_news(
     max_results: int = 10,
     country: str | None = None,
     language: str | None = "english",
-    start_date: str | None = None,
-    end_date: str | None = None,
+    start_date: str | None = "2012-01-01",
+    end_date: str | None = "2023-10-12",
 ):
     """
     Searches for news articles based on a query, and returns the top 10 results by defaults.
@@ -62,8 +77,104 @@ def get_news(
     if end_date is not None:
         new_rss_feed.end_date = end_date
     results = new_rss_feed.get_news(query)
-    results = [get_article(result["url"]) for result in results]
+    results = [get_article(result["url"].split("continue=")[-1]) for result in results]
     return results
+
+
+@app.get("/search_string_from_article/")
+def get_search_string(article: str):
+    prompt = (
+        article
+        + """
+    ----
+    Given the above news article, give me a search string that I can use to search for other articles on this topic. """
+    )
+
+    response = model.predict(prompt, **parameters)
+    return response
+
+
+@app.get("/score/")
+def do_the_prediction(article1: str):
+    prompt_text = (
+        article1
+        + """
+    ---
+    Score the two articles, if there are two, or the single article if there is only one based on the below <Scoring Criteria>, giving
+    each criteria a score, a high score means a strong bias on the criteria, low
+    score means little bias on the criteria. 
+
+    <Scoring Criteria>:
+        - **Word Choice and Tone**: Analyzing the words and tone used to see if they are neutral or emotionally charged to favor a certain perspective.
+        - **Framing**: Assessing how the news story is framed to identify if it presents a particular viewpoint or narrative.
+        - **Attribution and Sources**: Evaluating the variety and credibility of the sources cited, and whether they represent diverse perspectives.
+        - **Omission of Information**: Checking for any crucial information that is left out, which could help in understanding the complete story.
+        - **Misleading Statistics**: Evaluating the accuracy and representation of statistics to ensure they are not misleading or taken out of context.
+        - **Balance**: Checking if a news story presents multiple sides of an issue or predominantly favors one side.
+        - **Context**: Ensuring the news provides enough background information for readers to understand the issue.
+        - **Source Diversity**: Looking at the range and diversity of the sources cited in the news story.
+        - **Headline Accuracy**: Analyzing if the headline accurately represents the content of the article.
+        - **Use of Expertise**: Evaluating whether the article relies on recognized experts and authorities.
+        - **Transparency**: Checking for transparency about the news outlet’s processes, funding, and any potential conflicts of interest.
+        - **Historical Accuracy**: Evaluating whether historical facts are accurately presented and not distorted.
+        - **Image and Graphic Representation**: Assessing if visuals are used in a fair and accurate manner, or if they are manipulated to mislead.
+        - **Editorial Policies**: Understanding the editorial policies of the news outlet, including how they handle corrections and responses to criticism.
+        - **Comparative Analysis**: Comparing the coverage of the same issue across different news outlets to identify inconsistencies or biases.
+
+    For each article, first decide which 6 of the above <Scoring Criteria> are the most relevant. Then, create output where your output should be formatted as JSON. It should contain an array with one element for each article and each element should contain each one of the 5 <Scoring Criteria> you deemed relvant as a key (replace spaces by underscore) and the value should be the score (from 1-10). 
+    If only one article is provided then the array should only contain one element.
+    Do not add any additional strings or characters to the output so that the output can be parsed directly with the python json parser function json.loads().
+    """
+    )
+    response = model.predict(prompt_text, **parameters)
+    te = response.text
+    tt = te.split("[")[1].split("]")[0]
+    x = json.loads(tt)
+    return f"{x}"
+
+
+@app.get("/compare/")
+def do_the_prediction(article1: str, article2: str = ""):
+    prompt_text = (
+        article1
+        + """
+    ------
+    """
+        + article2
+        + """
+    ---
+    Score the two articles, if there are two, or the single article if there is only one based on the below <Scoring Criteria>, giving
+    each criteria a score, a high score means a strong bias on the criteria, low
+    score means little bias on the criteria. 
+
+    <Scoring Criteria>:
+        - **Word Choice and Tone**: Analyzing the words and tone used to see if they are neutral or emotionally charged to favor a certain perspective.
+        - **Framing**: Assessing how the news story is framed to identify if it presents a particular viewpoint or narrative.
+        - **Attribution and Sources**: Evaluating the variety and credibility of the sources cited, and whether they represent diverse perspectives.
+        - **Omission of Information**: Checking for any crucial information that is left out, which could help in understanding the complete story.
+        - **Misleading Statistics**: Evaluating the accuracy and representation of statistics to ensure they are not misleading or taken out of context.
+        - **Balance**: Checking if a news story presents multiple sides of an issue or predominantly favors one side.
+        - **Context**: Ensuring the news provides enough background information for readers to understand the issue.
+        - **Source Diversity**: Looking at the range and diversity of the sources cited in the news story.
+        - **Headline Accuracy**: Analyzing if the headline accurately represents the content of the article.
+        - **Use of Expertise**: Evaluating whether the article relies on recognized experts and authorities.
+        - **Transparency**: Checking for transparency about the news outlet’s processes, funding, and any potential conflicts of interest.
+        - **Historical Accuracy**: Evaluating whether historical facts are accurately presented and not distorted.
+        - **Image and Graphic Representation**: Assessing if visuals are used in a fair and accurate manner, or if they are manipulated to mislead.
+        - **Editorial Policies**: Understanding the editorial policies of the news outlet, including how they handle corrections and responses to criticism.
+        - **Comparative Analysis**: Comparing the coverage of the same issue across different news outlets to identify inconsistencies or biases.
+
+    For each article, first decide which 6 of the above <Scoring Criteria>  are the most relevant. Then, create output where your output should be formatted as JSON. It should contain an array with one element for each article and each element should contain each one of the 5 <Scoring Criteria> you deemed relvant as a key (replace spaces by underscore) and the value should be the score (from 1-10). 
+    If only one article is provided then the array should only contain one element.
+    Do not add any additional strings or characters to the output so that the output can be parsed directly with the python json parser function json.loads().
+    """
+    )
+    response = model.predict(prompt_text, **parameters)
+    te = response.text
+    tt = te.split("[")[1].split("]")[0]
+    tt = "[" + tt + "]"
+    x = json.loads(tt)
+    return f"{x}"
 
 
 @app.get("/read_article/{url:path}")
